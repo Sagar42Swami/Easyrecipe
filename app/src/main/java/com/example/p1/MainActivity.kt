3bb81e8ab694b63fb65017f71e3c0d859506c1d0
Sagar Swami <sagarswami4530@gmail.com>
2025-02-10 01:35:52 +0530

    Added second activity

--- app/src/main/java/com/example/p1/MainActivity.kt
+++ app/src/main/java/com/example/p1/MainActivity.kt
@@ -5,17 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
-        {v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val imageView: ImageView = this.findViewById(R.id.imageView)
+
+        // Set an OnClickListener on the ImageView
+        imageView.setOnClickListener {
+            // Create an Intent to start SecondActivity
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+            { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
